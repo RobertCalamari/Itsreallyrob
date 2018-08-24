9013f67514ddf185745b46493960695ab494368b
Robert Calamari <36252593+RobertCalamari@users.noreply.github.com>
2018-08-24 16:56:08 -0400

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,13 +1,20 @@
-'use strict';
+function determineSize(){
+	const vpWidth  = document.documentElement.clientWidth;
+	if(vpWidth >= 600)
+	{
+		document.getElementById('header').innerHTML="<div class='menu' >	
+				<img src='./img/CalamariWhite2.png' class='menubutt' alt='IRR' height='80' width='80' onclick='goHome()'> 
+			</div>			
+			<div class='title'>
+				<img src='./img/Logo.png' class='menubutt' alt='IRR' height='80' width='240' onclick='goHome()'> 
+			</div>
+			<div class='menu'>
+				<img src='./img/hambmenu2.png' class='menubutt' alt='Menu' height='70' width='40'  onclick='openNav()'> 
+			</div>	";
+	}
+	else
+	{
+		document.getElementById('header').innerHTML="Too small";
+	}
 
-const e = React.createElement;
-
-const list =
-  React.createElement('div', {},
-    React.createElement('div', {className:'menu'}, 
-		React.createElement('img', {src:'./img/CalamariWhite2.png', className:'menubutt', alt:'IRR', height:'80', width:'80', onClick:goHome()},
-		)
-	)
-  );
-
-ReactDOM.render(list, document.querySelector('#like_button_container'));
+}
